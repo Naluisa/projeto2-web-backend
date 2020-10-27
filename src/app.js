ecdd2e8fb25bd15cefbf60edffc7b2899e5f409f
Yuki <yukarihirako.13@gmail.com>
2020-10-27 20:35:23 -0300

    Fix heroku port

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    res.send("Hello world22");
+    res.send("Hello world");
 });
 
 app.listen(port, () => {
