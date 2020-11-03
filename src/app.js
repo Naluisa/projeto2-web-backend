e28bd7efe42f94768bff94f260040ce6d19d3169
Yuki <yukarihirako.13@gmail.com>
2020-11-03 15:24:04 -0300

    Add middlewares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routs = require("./routes");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
-});
+mongoose.connect(
+    process.env.MONGO_CONN ||
+        "mongodb+srv://admin:admin1@cluster0.n8sc6.mongodb.net/Project3?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(routs);
 
 app.listen(port, () => {
     console.log(`sever is running in port: ${port}`);
